62dcf8c9ccea91f3d69f74191f4b1d4e8b918129
hayley-c <hj.chung24@gmail.com>
2025-05-21 16:25:00 +1200

    Layers code
    Using layers code in order to run 2 different processes on the image, desaturating background & rectangles on flowers.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let curLayer = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -26,41 +27,56 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<4000;i++) {
-    colorMode(RGB);
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pixData = sourceImg.get(x, y);
-    let col = color(pixData);
-    let maskData = maskImg.get(x, y);
-    fill(pixData);
+  if(curLayer == 0){
+    for(let row=0;row<width;row++) {
+        for(let col = 0; col < height; col++){
+        let x = row;
+        let y = col; 
+        let pixData = sourceImg.get(x, y);
+        let c = color(pixData);
 
-    colorMode(HSB, 360, 100, 100);
-    let h = hue(col);
-    let s = saturation(col);
-    let b = brightness(col);
+        colorMode(HSB, 360, 100, 100);
+        let b = brightness(c);
 
-    if(maskData[0] > 128) {
-      let pointSize = 15;
-    rect(x, y, pointSize, pointSize);  
+        let newBrightness = map(b, 0, 100, 50, 100);
+        let newColour = color(0, 0, newBrightness);
+
+        fill(newColour);
+        set(x, y,newColour);
+      }
     }
-    else { 
-      let newBrightness = map(b, 0, 100, 50, 100);
-      let newColour = color(0, 0, newBrightness);
-      let pointSize = 5;
-      fill(newColour)
-      ellipse(x, y, pointSize, pointSize);
+    updatePixels();
+  }
+
+  if(curLayer == 1){
+    for(let i=0;i<4000;i++) {
+      colorMode(RGB);
+      let x = floor(random(sourceImg.width));
+      let y = floor(random(sourceImg.height));
+      let pixData = sourceImg.get(x, y);
+      let maskData = maskImg.get(x, y);
+      fill(pixData);
+
+      if(maskData[0] > 128) {
+        let pointSize = 15;
+        rect(x, y, pointSize, pointSize);  
+      }
     }
   }
-}
-
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 1 && curLayer == 0) {
+  renderCounter = 0; 
+  curLayer = 1;
+  console.log("change to layer 1")
+  }
+
+  if(renderCounter > 10 && curLayer == 1) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
+}
 
 function keyTyped() {
   if (key == '!') {
