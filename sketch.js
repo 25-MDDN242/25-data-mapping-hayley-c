c073a7700df02b829a70d36a93bc2970d027afcd
hayley-c <hj.chung24@gmail.com>
2025-06-08 21:46:51 +1200

    flower label based on input
    tried to use mask data colour to determine the label text, but couldn't figure it out so text is based on input

--- sketch.js
+++ sketch.js
@@ -6,11 +6,14 @@ let maskCenter = null;
 let maskCenterSize = null; 
 let barcode = [];
 
+let isDaisy = false;
+let isRose = false; 
+
 let loopCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile = "mask_1.png";
+let sourceFile = "input_7.jpg";
+let maskFile = "mask_7.png";
 let outputFile = "output_5.png";
 
 function preload() {
@@ -31,6 +34,32 @@ function setup () {
 
   maskCenterSearcher(20);
   maskSizeFinder(20);
+
+  switch(sourceFile){
+    case "input_1.jpg":
+    case "input_2.jpg":
+    case "input_3.jpg":
+    case "input_4.jpg":
+    case "input_5.jpg":
+    case "input_new1.jpg":
+    case "input_new2.jpg":
+    case "input_new3.jpg":
+      isRose = true;
+      isDaisy = false;
+      break;
+
+    case "input_6.jpg":
+    case "input_7.jpg":
+    case "input_8.jpg":
+    case "input_9.jpg":
+    case "input_10.jpg":
+    case "input_new4.jpg":
+    case "input_new5.jpg":
+    case "input_new6.jpg":
+      isRose = false;
+      isDaisy = true;
+      break;
+  }
 }
 
 let X_STOP = 1920;
@@ -159,32 +188,13 @@ function draw () {
       let pixData = sourceImg.get(x1, y1);
       let maskData = maskImg.get(x1, y1);
 
-      // copy image regions 
-      let sourceWidth = random(25, 50); // copy source width
-      let sourceHeight = random(25, 50); // copy source height
-      let destWidth = random(50, 200); // copy destination width 
-      let destHeight = random(50, 200); // copy destination height
-      noStroke();
-
-
       if(maskData[1] > 128) {
-        imageMode(CORNERS);
-
-        fill(pixData); // image pixels
-        copy(sourceImg, x1, y1, sourceWidth, sourceHeight, x2, y2, destWidth, destHeight); // copy source region to new destination with new size 
-
-        colorMode(HSB, 360, 100, 100, 100);
-        let hueValue = hue(maskCenterPixel); // hue of mask centre pixel
-        let mapHue = map(hueValue, 0, 360, 0, 120); // 
-        let oppositeHue = 300 - mapHue; // find complementary hue
-        let oppositeOpaque = color(oppositeHue, 100, 100, 100); // complementary colour
-        let oppositeTransparent = color(oppositeHue, 100, 100, 75); // complementary colour
-
-      //   // random solid colour rectangle
-        fill(oppositeTransparent);
-        rectTint(x1, y1);
-        fill(oppositeOpaque);
-        barcodes(x1, y1);
+        // random image regions copies
+        maskGlitch(x1, y1, x2, y2, pixData);
+        // opposite tint rectangle at random x and y coordinates
+        rectTint(x1, y1, maskCenterPixel);
+        // opposite colour barcodes at random x and y coordinates
+        barcodes(x1, y1, maskCenterPixel);
       }
     }
     renderCounter = renderCounter + 1;
@@ -192,30 +202,43 @@ function draw () {
  
   // mask scanner focus rectangle
   if (maskCenter !== null) {
-    let flowerType = 'daisy'
-    // focus rectangle
+
+    // identify flower type
+    let flowerType = null;
+    if (isRose == true){
+      flowerType = "rose";
+    }
+    else {
+      flowerType = "daisy";
+    }
+
+    let boxLeft = maskCenter[0] - maskWidth/2; // flower bounding box left x coordinate
+    let boxTop = maskCenter[1] - maskHeight/2; // flower bounding box top y coordinate
+    let labelLeft = boxLeft - 4; // flower type label background left x coordinate
+    let labelTop = boxTop - 30; // flower type label background top y coordinate
+
+    // bounding box
     colorMode(RGB);
-    rectMode(CENTER);
-    strokeWeight(8); // scan line weight
-    stroke(255, 255, 255); // white scan lines
+    rectMode(CENTER); // bounding box from mask centre
+    strokeWeight(8); // bounding box line weight
+    stroke(255, 255, 255); // white bounding box
     noFill();
-    rect(maskCenter[0], maskCenter[1], maskWidth, maskHeight); // scan rectangle
+    rect(maskCenter[0], maskCenter[1], maskWidth, maskHeight); // bounding box rectangle
 
-    // label background
-    let labelWidth = textWidth(flowerType) + 5; // label backing width 
-    rectMode(CORNER);
+    // flower type label background
+    let labelWidth = textWidth(flowerType) + 10; // label backing width 
+    rectMode(CORNER); // label from top left corner
     noStroke();
-    fill(maskCenterPixel); // centre mask pixel colour
-    rect(maskCenter[0] - maskWidth/2 - 2, maskCenter[1] - maskHeight/2 - 30, labelWidth, 24); // label backing
+    fill(maskCenterPixel); // mask's centre pixel's colour
+    rect(labelLeft, labelTop, labelWidth, 24); // label backing
 
-    // label text
-    rectMode(CENTER);
+    // flower type label text
     noFill();
     stroke(255, 255, 255); // white text
-    strokeWeight(2); // text weight
-    textSize(24); // text size
+    strokeWeight(1.75); // font weight
+    textSize(24); // font size
     textFont('Courier New'); // courier new typeface
-    text(flowerType, maskCenter[0] - maskWidth/2, maskCenter[1] - maskHeight/2 - 10); // text and text location 
+    text(flowerType, boxLeft, boxTop - 12); // flower type label located at the top left of the flower bounding boz
   }
   renderCounter = renderCounter + 1;
 
@@ -238,21 +261,54 @@ function keyTyped() {
   }
 }
 
-function rectTint(x1, y1){
+// random image regions copies
+function maskGlitch(x1, y1, x2, y2, pixData){
+  // copy image regions 
+  let sourceWidth = random(25, 50); // copy source width
+  let sourceHeight = random(25, 50); // copy source height
+  let destWidth = random(50, 200); // copy destination width 
+  let destHeight = random(50, 200); // copy destination height
+
+  imageMode(CORNERS);
+  noStroke();
+  fill(pixData);
+  copy(sourceImg, x1, y1, sourceWidth, sourceHeight, x2, y2, destWidth, destHeight); // copy source region to new destination with new size 
+}
+
+// opposite tint rectangle at random x and y coordinates
+function rectTint(x1, y1, maskCenterPixel){
   let tintX = random (-50, 50);
   let tintY = random (-100, 100);
+  colorMode(HSB, 360, 100, 100, 100);
+  let hueValue = hue(maskCenterPixel); // mask's centre pixel's hue
+  let mapHue = map(hueValue, 0, 360, 0, 120); // map mask's centre pixel's hue
+  let oppositeHue = 300 - mapHue; // find opposite hue
+  let oppositeTint = color(oppositeHue, 100, 100, 75); // opposite colour tint
+
+  rectMode(CENTER);
+  noStroke();
+  fill(oppositeTint);
+
   push();
   translate(tintX, tintY);
-  rectMode(CENTER);
   let tintWidth = random(50, 250);
   let tintHeight = random(5, 100);
   rect(x1, y1, tintWidth, tintHeight);
   pop();
 }
 
-function barcodes(x1, y1){
+// opposite colour barcodes at random x and y coordinates
+function barcodes(x1, y1, maskCenterPixel){
   let barcodeX = random (-60, 30);
   let barcodeY = random (-20, 10);
+  colorMode(HSB, 360, 100, 100, 100);
+  let hueValue = hue(maskCenterPixel); // mask's centre pixel's hue
+  let mapHue = map(hueValue, 0, 360, 0, 120); // map mask's centre pixel's hue
+  let oppositeHue = 300 - mapHue; // find opposite hue
+  let oppositeColour = color(oppositeHue, 100, 100, 100); // opposite colour tint
+  noStroke();
+  fill(oppositeColour);
+
   push();
   let barcodeSize = random(75, 125);
   let gaps = random(18, 23);
@@ -268,39 +324,3 @@ function barcodes(x1, y1){
   }
   pop();
 }
-
-function flowerLabel(){
-
-      // let x1 = random(0, width);
-    // let y1 = random(0, height);
-    // let maskData = maskImg.get(x1, y1);
-    // if (maskData[1] > 218 && maskData[1] < 236){
-    //   let flowerType = 'rose'
-    // }
-    // else if(maskData[1] > 236){
-    //   let flowerType = 'daisy'
-    // }
-  colorMode(RGB);
-    rectMode(CENTER);
-    strokeWeight(5); // scan line weight
-    stroke(255, 255, 255); // white scan lines
-    noFill();
-    rect(maskCenter[0], maskCenter[1], maskWidth, maskHeight); // scan rectangle
- 
-    // label background
-    let labelWidth = textWidth('rose') + 2; // label backing width 
-    rectMode(CORNER);
-    noStroke();
-    fill(maskCenterPixel); // centre mask pixel colour
-    rect(maskCenter[0] - maskWidth/2 - 2, maskCenter[1] - maskHeight/2 - 30, labelWidth, 24); // label backing
-
-    // label text
-    rectMode(CENTER);
-    noFill();
-    stroke(255, 255, 255); // white text
-    strokeWeight(2); // text weight
-    textSize(24); // text size
-    textFont('Courier New'); // courier new typeface
-    text('rose', maskCenter[0] - maskWidth/2, maskCenter[1] - maskHeight/2 - 10); // text and text location 
-}
-
